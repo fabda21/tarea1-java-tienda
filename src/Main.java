7509e8e36df1b2a102c24e16a592255776ad1a61
fabda21 <fadamuor@gmail.com>
2025-08-03 11:44:50 -0600

    Implementado menú interactivo con switch y bucle while

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,40 @@ public class Main {
         String[] productos = {"Camisa", "Pantalón", "Zapatos", "Gorra", "Mochila"};
         double[] precios = {350.0, 550.0, 1200.0, 180.0, 700.0};
         boolean[] carrito = new boolean[productos.length];
+        int opcion;
+        do {
+            System.out.println("\n==== MENÚ DE TIENDA ====");
+            System.out.println("1. Ver productos disponibles");
+            System.out.println("2. Agregar producto al carrito");
+            System.out.println("3. Ver total a pagar");
+            System.out.println("4. Salir");
+            System.out.print("Seleccione una opción: ");
+            opcion = scanner.nextInt();
 
+            switch (opcion) {
+                case 1:
+                    System.out.println("\n--- Lista de Productos ---");
+                    for (int i = 0; i < productos.length; i++) {
+                        System.out.println((i + 1) + ". " + productos[i] + " - L " + precios[i]);
+                    }
+                    break;
+
+                case 2:
+                    System.out.println("Funcionalidad en desarrollo...");
+                    break;
+
+                case 3:
+                    System.out.println("Funcionalidad en desarrollo...");
+                    break;
+
+                case 4:
+                    System.out.println("Gracias por visitarnos.");
+                    break;
+
+                default:
+                    System.out.println("Opción inválida.");
+            }
+
+        } while (opcion != 4);
     }
 }
