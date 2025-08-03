f78aff396eb2a2f5add7221253e99208f9f53766
fabda21 <fadamuor@gmail.com>
2025-08-03 09:04:43 -0600

    Estructura base del proyecto y arreglos definidos
    # Conflicts:
    #	.gitignore

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+        String[] productos = {"Camisa", "Pantalón", "Zapatos", "Gorra", "Mochila"};
+        double[] precios = {350.0, 550.0, 1200.0, 180.0, 700.0};
+        boolean[] carrito = new boolean[productos.length];
+
+    }
+}
